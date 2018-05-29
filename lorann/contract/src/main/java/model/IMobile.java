b0fce4f75f8bc38e034cb40d3677182bdce324af
DESKTOP-P9K4E6I\chris <chris.chich@hotmail.fr>
2018-05-29 16:42:43 +0200

    IMobile done

--- lorann/contract/src/main/java/model/IMobile.java
+++ lorann/contract/src/main/java/model/IMobile.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.awt.Point;
+
 public interface IMobile extends IElement{
+	
+	void moveUp();
+	void moveLeft();
+	void moveDown();
+	void moveRight();
+	void doNothing();
+	int getX();
+	int getY();
+	boolean isAlive();
+	boolean isHit();
+	boolean isOnDoor();
+	boolean isOnKey();
+	Point getPosition();
+	
 
 }
