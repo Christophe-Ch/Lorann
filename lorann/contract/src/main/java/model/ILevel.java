2fe001caa5101549fdbc8ba544610f5e5b435bd0
DESKTOP-P9K4E6I\chris <chris.chich@hotmail.fr>
2018-05-29 17:21:10 +0200

    Moved interfaces from model module to contract module

--- lorann/contract/src/main/java/model/ILevel.java
+++ lorann/contract/src/main/java/model/ILevel.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.util.Observable;
+
 public interface ILevel {
+	
+	int getWidth();
+	int getHeight();
+	IElement getOnTheLevelXY(int x, int y);
+	Observable getObservable();
+	void setElementHasChanged();
 
 }
