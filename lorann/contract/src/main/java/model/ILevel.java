5dc95129525d7b00b468f99ccbd7eeb223e1b7d8
CorentinHangard <corentin.hangard@viacesi.fr>
2018-05-30 12:24:09 +0200

    Test View

--- lorann/contract/src/main/java/model/ILevel.java
+++ lorann/contract/src/main/java/model/ILevel.java
@@ -7,6 +7,7 @@ public interface ILevel {
 	int getWidth();
 	int getHeight();
 	IElement getOnTheLevelXY(int x, int y);
+	IElement setOnTheLevelXY(int x, int y);
 	Observable getObservable();
 	void setElementHasChanged();
 
--- lorann/view/src/main/java/test/ViewFacadeTest.java
+++ lorann/view/src/main/java/test/ViewFacadeTest.java
@@ -2,25 +2,44 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.awt.AWTException;
+import java.awt.Button;
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import controller.IOrderPerformer;
+import model.IElement;
 import model.ILevel;
 import model.IMobile;
+import model.Permeability;
+import model.Sprite;
 import view.ViewFacade;
 
 public class ViewFacadeTest {
-
+	
 	private ILevel level;
 	private IMobile myCharacter;
 	private ViewFacade view;
+	private Rectangle fullView;
 	
 	@Before
 	public void setUp() throws Exception {
+		level = new LevelMock();
+		myCharacter = new MobileMock();
 		view = new ViewFacade(level, myCharacter);
+		fullView = new Rectangle(0, 0, level.getWidth(), level.getHeight());
 	}
 
 	@After
@@ -33,23 +52,9 @@ public class ViewFacadeTest {
 	}
 
 	@Test
-	public void testRun() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testKeyTyped() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testKeyPressed() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testKeyReleased() {
-		fail("Not yet implemented");
+	public void testKeyPressed() throws AWTException {
+		Robot robot = new Robot();
+		robot.keyPress(KeyEvent.VK_RIGHT);
 	}
 
 	@Test
@@ -58,18 +63,22 @@ public class ViewFacadeTest {
 	}
 
 	@Test
-	public void testSetLevel() {
-		fail("Not yet implemented");
+	public void testSetLevel() throws IOException {
+		final ILevel expected = new LevelMock();
+		this.view.setLevel(expected);
+		assertEquals(expected, this.view.getLevel());
 	}
 
 	@Test
 	public void testGetMyCharacter() {
-		fail("Not yet implemented");
+		assertEquals(this.myCharacter, this.view.getMyCharacter());
 	}
 
 	@Test
 	public void testSetMyCharacter() {
-		fail("Not yet implemented");
+		final IMobile expected = new MobileMock();
+		this.view.setMyCharacter(expected);
+		assertEquals(expected, this.view.getMyCharacter());
 	}
 
 	@Test
@@ -79,12 +88,14 @@ public class ViewFacadeTest {
 
 	@Test
 	public void testGetFullView() {
-		fail("Not yet implemented");
+		assertEquals(fullView, this.view.getFullView());
 	}
 
 	@Test
 	public void testSetFullView() {
-		fail("Not yet implemented");
+		final Rectangle expected = new Rectangle(0, 0, level.getWidth(), level.getHeight());
+		this.view.setFullView(expected);
+		assertEquals(expected, this.view.getFullView());
 	}
 
 }
