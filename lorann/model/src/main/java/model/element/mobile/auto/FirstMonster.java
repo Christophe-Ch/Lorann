d9c9dc4bf578c5e4c1d237bfbbb88b174d69d8d2
DESKTOP-P9K4E6I\chris <chris.chich@hotmail.fr>
2018-05-30 22:12:28 +0200

    Added permeabilities for monsters and spell

---
+++ lorann/model/src/main/java/model/element/mobile/auto/FirstMonster.java
@@ -0,0 +1,19 @@
+package model.element.mobile.auto;
+
+import model.ILevel;
+import model.Sprite;
+
+public class FirstMonster extends Monster {
+
+	public FirstMonster(Sprite sprite, ILevel level, int x, int y) {
+		super(sprite, level, x, y);
+	}
+
+	@Override
+	public void doNothing() {
+		// TODO Auto-generated method stub
+	}
+
+	
+
+}
--- lorann/model/src/main/java/model/element/mobile/auto/Spell.java
+++ lorann/model/src/main/java/model/element/mobile/auto/Spell.java
@@ -1,7 +1,26 @@
 package model.element.mobile.auto;
 
+import model.ILevel;
+import model.Permeability;
+import model.Sprite;
 import model.element.mobile.Mobile;
 
 public class Spell extends Mobile{
+
+	private static Sprite first_sprite;
+	private static Sprite second_sprite;
+	private static Sprite third_sprite;
+	private static Sprite fourth_sprite;
+	private static Sprite fifth_sprite;
+	
+	public Spell(ILevel level, int x, int y) {
+		super(first_sprite, Permeability.SPELL, level, x, y);
+	}
+
+	@Override
+	public void doNothing() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
