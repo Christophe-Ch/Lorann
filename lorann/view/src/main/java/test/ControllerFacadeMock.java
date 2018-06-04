2ebeb1c8fde0ae98c5a5143a08c299448c57b6a3
CorentinHangard <33125223+CorentinHangard@users.noreply.github.com>
2018-06-04 13:11:29 +0200

    Update ControllerFacadeMock.java

--- lorann/view/src/main/java/test/ControllerFacadeMock.java
+++ lorann/view/src/main/java/test/ControllerFacadeMock.java
@@ -4,33 +4,22 @@ import java.awt.event.KeyEvent;
 
 import controller.IOrderPerformer;
 
-<<<<<<< HEAD
-public class ControllerFacadeMock implements IOrderPerformer {
-=======
 /**
  * <h1>The Class ViewFacade provides a facade of the View component.</h1>
  *
- * @author Corentin Hangard & Christophe Chichmanian & Charlotte Bénard & François Mouton
+ * @author Corentin Hangard & Christophe Chichmanian & Charlotte BÃ©nard & FranÃ§ois Mouton
  * @version 1.0
  */
 public class ControllerFacadeMock implements IOrderPerformer{
->>>>>>> branch 'master' of https://github.com/ChriisX/Lorann
 
-<<<<<<< HEAD
-	
-	public ControllerFacadeMock() {
-	}
-	
-=======
 	/*
      * (non-Javadoc)
      * @see view.IView#displayMessage(java.lang.String)
      */
->>>>>>> branch 'master' of https://github.com/ChriisX/Lorann
 	@Override
 	public void performOrder(KeyEvent userOrder) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
